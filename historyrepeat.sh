58140801e1ebf02a4ee662116046805121bd7829
HotSushi <sushant.s.raikar@gmail.com>
2017-11-10 00:27:04 +0000

    ager script

---
+++ historyrepeat.sh
@@ -0,0 +1,6 @@
+input="/home/ubuntu/rhistory"
+while IFS= read -r var
+do
+  git cherry-pick "$var" --strategy-option theirs --keep-redundant-commits --allow-empty-message --allow-empty -x --no-commit
+  git commit -a --allow-empty-message -m ''
+done < "$input"
