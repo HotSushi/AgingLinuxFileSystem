dccc1786b5bc058537075294a37c3673e27b89cd
Sushant Raikar <sushantthecoder@gmail.com>
2017-11-12 11:32:18 -0500

    delete line by line

--- historyrepeat.sh
+++ historyrepeat.sh
@@ -1,6 +1,8 @@
 input=$1
+cp input $1.backup
 while IFS= read -r var
 do
   git cherry-pick "$var" --strategy-option theirs --keep-redundant-commits --allow-empty-message --allow-empty -x --no-commit
   git commit -a --allow-empty-message -m ''
+  sed -i.bak -e '1d' $1.backup
 done < "$input"
