70ac03e647bc1326396280f229ba25efc647677b
Sushant Raikar <sushantthecoder@gmail.com>
2017-11-12 11:16:30 -0500

    CommandlineArgument

--- historyrepeat.sh
+++ historyrepeat.sh
@@ -1,4 +1,4 @@
-input="/home/ubuntu/rhistory"
+input=$1
 while IFS= read -r var
 do
   git cherry-pick "$var" --strategy-option theirs --keep-redundant-commits --allow-empty-message --allow-empty -x --no-commit
