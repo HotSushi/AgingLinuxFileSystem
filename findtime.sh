dc40cef613d1edea7c6c6be7d14a92b89182948e
HotSushi <sushant.s.raikar@gmail.com>
2017-11-13 13:59:15 -0500

    modified scripts

--- findtime.sh
+++ findtime.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
+for i in {1..100}
+do
 start=`date +%s%N`
-ls -l ~/repo/linux > /dev/null
+find $1 -name ghtgfsgd!!dfdsfs
 end=`date +%s%N`
 diff=`expr ${end} - ${start}`
 h=`expr ${diff}/1000000000.0 | bc -l`
 echo "Total Time - $h"
+done
--- many.sh
+++ many.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 for i in  {1..1000}
 do
-    ./caltime.sh linux/README >> ~/before/new/readme.time
-    ./caltime.sh linux/* >> ~/before/new/all.time
-    ./createdir.sh >> ~/before/new/createdir.time
-    ./removedir.sh >> ~/before/new/removedir.time
-    ./lstime.sh >> ~/before/new/lsdir.time
+    ./caltime.sh ~/repo/linux/README >> ~/before/readme.time
+    ./caltime.sh ~/repo/linux/* >> ~/before/all.time
+    ./createdir.sh >> ~/before/createdir.time
+    ./removedir.sh >> ~/before/removedir.time
+    ./lstime.sh >> ~/before/lsdir.time
 done
-./writetime.sh >> ~/before/new/write.time
+./writetime.sh >> ~/before/write.time
+./findtime.sh ~/repo/linux >> ~/before/findl.time
