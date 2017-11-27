e48aab0ec2a8302981a2a7529b958adf874bfaf3
Ubuntu <ubuntu@ugster08.student.cs.uwaterloo.ca>
2017-11-26 20:07:02 -0500

    merge error hadnled

--- historyrepeat.sh
+++ historyrepeat.sh
@@ -2,7 +2,14 @@ input=$1
 cp $1 $1.backup
 while IFS= read -r var
 do
-  git cherry-pick "$var" --strategy-option theirs --keep-redundant-commits --allow-empty-message --allow-empty -x --no-commit
-  git commit -a --allow-empty-message -m ''
+  count_parents=$(git cat-file -p $var | grep -oc parent)
+  if (($count_parents > 1)); then
+       echo  "need to merge"
+       git cherry-pick "$var" -m 1 --strategy-option theirs --keep-redundant-commits --allow-empty-message --allow-empty -x --no-commit
+       git commit -a --allow-empty-message -m ''
+  else
+       git cherry-pick "$var" --strategy-option theirs --keep-redundant-commits --allow-empty-message --allow-empty -x --no-commit
+       git commit -a --allow-empty-message -m ''
+  fi
   sed -i.bak -e '1d' $1.backup
 done < "$input"
